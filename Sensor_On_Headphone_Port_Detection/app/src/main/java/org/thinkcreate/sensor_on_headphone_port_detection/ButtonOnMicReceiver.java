a5a7da049bcca161d7781a73cab22806f0dc46e0
Deqing Sun <iamseer@gmail.com>
2016-01-03 15:43:21 -0500

    functional receive

---
+++ Sensor_On_Headphone_Port_Detection/app/src/main/java/org/thinkcreate/sensor_on_headphone_port_detection/ButtonOnMicReceiver.java
@@ -0,0 +1,50 @@
+package org.thinkcreate.sensor_on_headphone_port_detection;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.widget.Toast;
+
+public class ButtonOnMicReceiver extends BroadcastReceiver {
+    static final String TAG = "ButtonOnMicReceiver";
+
+    public ButtonOnMicReceiver() {
+        super();
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        String action = intent.getAction();
+
+        Log.d(TAG, "onReceive");
+
+        if (action.equals(Intent.ACTION_HEADSET_PLUG)) {
+            //Toast.makeText(context, "earphones activity",Toast.LENGTH_SHORT).show();
+            if (intent.getExtras().getInt("state")==1) {//if plugged
+                Toast.makeText(context, "Headphone plugged", Toast.LENGTH_LONG).show();
+                Log.d(TAG, "Headphone plugged");
+            } else {
+                Toast.makeText(context, "Headphone un-plugged",Toast.LENGTH_LONG).show();
+                Log.d(TAG, "Headphone un-plugged");
+            }
+        }else if (action.equals(Intent.ACTION_MEDIA_BUTTON)) {
+            KeyEvent keyEvent  = (KeyEvent)intent.getParcelableExtra(Intent.EXTRA_KEY_EVENT);
+            //Log.d(TAG, keyEvent.toString());
+            if (keyEvent != null) {
+                if (keyEvent.getAction() == KeyEvent.ACTION_UP){
+                    switch(keyEvent.getKeyCode()) {
+                        case KeyEvent.KEYCODE_HEADSETHOOK:
+                            Log.d(TAG, "KEYCODE_HEADSETHOOK");
+                            break;
+                    }
+                }
+            }
+        }else{
+            Log.d(TAG, "unknown action: "+action);
+        }
+
+    }
+}
--- Sensor_On_Headphone_Port_Detection/app/src/main/java/org/thinkcreate/sensor_on_headphone_port_detection/MainActivity.java
+++ Sensor_On_Headphone_Port_Detection/app/src/main/java/org/thinkcreate/sensor_on_headphone_port_detection/MainActivity.java
@@ -1,13 +1,30 @@
 package org.thinkcreate.sensor_on_headphone_port_detection;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.media.AudioManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends ActionBarActivity {
+
+    static final String TAG = "MainActivity";
+    private ButtonOnMicReceiver mButtonOnMicReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButtonOnMicReceiver = new ButtonOnMicReceiver();
+        registerReceiver(mButtonOnMicReceiver, new IntentFilter(Intent.ACTION_HEADSET_PLUG));
+
+        AudioManager am = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
+        am.registerMediaButtonEventReceiver(new ComponentName(getPackageName(), ButtonOnMicReceiver.class.getName()));
+
+        Log.v(TAG, "HeadsetExample: The Button Receiver has been registered");
     }
 }

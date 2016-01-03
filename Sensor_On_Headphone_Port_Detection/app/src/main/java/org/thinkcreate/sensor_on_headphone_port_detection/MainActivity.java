d664bba95f3cb02e3a799be4133205ff5ef8387a
Deqing Sun <iamseer@gmail.com>
2016-01-03 11:10:25 -0500

    start empty project

---
+++ Sensor_On_Headphone_Port_Detection/app/src/main/java/org/thinkcreate/sensor_on_headphone_port_detection/MainActivity.java
@@ -0,0 +1,13 @@
+package org.thinkcreate.sensor_on_headphone_port_detection;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

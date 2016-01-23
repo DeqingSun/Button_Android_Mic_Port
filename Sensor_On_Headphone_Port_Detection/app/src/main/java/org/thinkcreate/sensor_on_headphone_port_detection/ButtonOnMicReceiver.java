4d8f340f0cbac56062452db3fcc0a36502675487
Deqing Sun <iamseer@gmail.com>
2016-01-23 16:20:50 -0500

    update

--- Sensor_On_Headphone_Port_Detection/app/src/main/java/org/thinkcreate/sensor_on_headphone_port_detection/ButtonOnMicReceiver.java
+++ Sensor_On_Headphone_Port_Detection/app/src/main/java/org/thinkcreate/sensor_on_headphone_port_detection/ButtonOnMicReceiver.java
@@ -9,6 +9,7 @@ import android.widget.Toast;
 
 public class ButtonOnMicReceiver extends BroadcastReceiver {
     static final String TAG = "ButtonOnMicReceiver";
+    static int counter = 0;
 
     public ButtonOnMicReceiver() {
         super();
@@ -30,6 +31,7 @@ public class ButtonOnMicReceiver extends BroadcastReceiver {
                 Toast.makeText(context, "Headphone un-plugged",Toast.LENGTH_LONG).show();
                 Log.d(TAG, "Headphone un-plugged");
             }
+            counter++;
         }else if (action.equals(Intent.ACTION_MEDIA_BUTTON)) {
             KeyEvent keyEvent  = (KeyEvent)intent.getParcelableExtra(Intent.EXTRA_KEY_EVENT);
             //Log.d(TAG, keyEvent.toString());
@@ -38,6 +40,7 @@ public class ButtonOnMicReceiver extends BroadcastReceiver {
                     switch(keyEvent.getKeyCode()) {
                         case KeyEvent.KEYCODE_HEADSETHOOK:
                             Log.d(TAG, "KEYCODE_HEADSETHOOK");
+                            counter++;
                             break;
                     }
                 }
@@ -45,6 +48,7 @@ public class ButtonOnMicReceiver extends BroadcastReceiver {
         }else{
             Log.d(TAG, "unknown action: "+action);
         }
+        Log.d(TAG, "count "+counter);
 
     }
 }
--- Sensor_On_Headphone_Port_Detection/app/src/main/java/org/thinkcreate/sensor_on_headphone_port_detection/MainActivity.java
+++ Sensor_On_Headphone_Port_Detection/app/src/main/java/org/thinkcreate/sensor_on_headphone_port_detection/MainActivity.java
@@ -8,6 +8,8 @@ import android.media.AudioManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -25,6 +27,37 @@ public class MainActivity extends ActionBarActivity {
         AudioManager am = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
         am.registerMediaButtonEventReceiver(new ComponentName(getPackageName(), ButtonOnMicReceiver.class.getName()));
 
+
         Log.v(TAG, "HeadsetExample: The Button Receiver has been registered");
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        // TODO Auto-generated method stub
+        if(keyCode== KeyEvent.KEYCODE_VOLUME_DOWN){
+
+            return true;
+        }
+        else if(keyCode==KeyEvent.KEYCODE_VOLUME_UP)
+        {
+
+            return true;
+        }
+        else return super.onKeyUp(keyCode, event);
+    }
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // TODO Auto-generated method stub
+        if(keyCode==KeyEvent.KEYCODE_VOLUME_DOWN){
+            Log.v(TAG, "down");
+            Toast.makeText(this, "KEYCODE_VOLUME_DOWN", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        else if(keyCode==KeyEvent.KEYCODE_VOLUME_UP)
+        {
+            Log.v(TAG, "up");
+            Toast.makeText(this, "KEYCODE_VOLUME_UP", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        else return super.onKeyDown(keyCode, event);
     }
 }
